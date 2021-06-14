67335d4e3623bb126e22d74a28c0d6c02995289e
PhilipvEgmond <philip.van.egmond@hva.nl>
2021-06-14 17:20:10 +0200

    fix favorieten en filter

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const multer = require("multer");
-const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // DB Setup
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 const uri = process.env.DB_KEY;
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 //afbeeldingen worden opgeslagen in de public/uploads map
 const storage = multer.diskStorage({
   destination: function (request, file, callback) {
-    callback(null, "./public/uploads");
+    callback(null, './public/uploads');
   },
 
   //afbeeldingen krijgen naast de oorspronkelijke naam ook de huidige datum
@@ -33,7 +33,7 @@ const storage = multer.diskStorage({
 //gewijzigde afbeeldingen
 const storageWijzig = multer.diskStorage({
   destination: function (request, file, callback) {
-    callback(null, "./public/uploads");
+    callback(null, './public/uploads');
   },
 
   filename: function (request, file, callback) {
@@ -58,15 +58,15 @@ const uploadWijzig = multer({
 });
 
 //de css, img en js map in de public map gebruiken
-app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public.css"));
-app.use("/img", express.static(__dirname + "public.img"));
-app.use("/js", express.static(__dirname + "public.js"));
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public.css'));
+app.use('/img', express.static(__dirname + 'public.img'));
+app.use('/js', express.static(__dirname + 'public.js'));
 
 //express layout mobiel formaat en ejs gebruiken
 app.use(expressLayouts);
-app.set("layout", "./layouts/mobiel-formaat");
-app.set("view engine", "ejs");
+app.set('layout', './layouts/mobiel-formaat');
+app.set('view engine', 'ejs');
 
 //bodyparser en express.json voor http requests
 app.use(express.json());
@@ -78,64 +78,122 @@ app.use(bodyParser.json());
 // --- routing ---
 
 // render index
-app.get("", (req, res) => {
-  res.render("index");
+app.get('', (req, res) => {
+  res.render('index');
 });
 
 //aanmelden route
-app.get("/aanmelden", (req, res) => {
-  res.render("aanmelden");
+app.get('/aanmelden', (req, res) => {
+  res.render('aanmelden');
 });
 
 //zoeken route en gebruikers/oproepen in database vinden en mee sturen naar zoeken pagina
-app.get("/zoeken", (req, res) => {
-  client.connect((err, db) => {
-    if (err) throw err;
-    db.db("TechTeam")
-      .collection("gebruikers")
-      .find()
-      .toArray()
-      .then((gebruikers) => {
-        res.render("zoeken", {
-          gebruikersLijst: gebruikers,
-        });
-      });
-  });
-});
+app.get('/zoeken', renderZoeken);
+
+// favorieten route
+app.get('/favorieten', renderFavorieten);
 
 //wijzigen route
-app.get("/wijzigen", (req, res) => {
-  res.render("wijzigen");
+app.get('/wijzigen', (req, res) => {
+  res.render('wijzigen');
 });
 
 //verwijderen route
-app.get("/verwijderen", (req, res) => {
-  res.render("verwijderen");
+app.get('/verwijderen', (req, res) => {
+  res.render('verwijderen');
 });
 
 //tutorial route
-app.get("/hoe-werkt-het", (req, res) => {
-  res.render("hoewerkthet");
+app.get('/hoe-werkt-het', (req, res) => {
+  res.render('hoewerkthet');
 });
 
 //error route
-app.get("/error", (req, res) => {
-  res.render("error");
+app.get('/error', (req, res) => {
+  res.render('error');
 });
 
 // api get
-app.get("/fortnite", handleApi);
+app.get('/fortnite', renderApi);
 
-//fortnie route
+// --- post ---
 
-async function handleApi(req, res) {
+// filter post
+app.post('/zoeken', handleZoeken);
+app.post('/favorieten', handleFavorietenVerwijderen);
+
+// -- routing functions --
+
+async function renderZoeken(req, res) {
+  try {
+    const client = new MongoClient(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    client.connect(async (err, db) => {
+      if (err) throw err;
+
+      const gebruikersCol = db.db('TechTeam').collection('gebruikers');
+      const favorietenCol = db.db('TechTeam').collection('favorieten');
+
+      // haal alle gebruikers op en opgeslagen gebruikers
+      let users = await gebruikersCol.find().toArray();
+      const favorites = await favorietenCol.findOne({ id: 0 });
+
+      // maak nieuwe array waar opgeslagen gebruikers niet instaan
+      let undiscoveredUsers = users.filter(gebruiker => {
+        return !favorites.opgeslagen.includes(gebruiker.naam);
+      });
+
+      res.render('zoeken', { gebruikersLijst: undiscoveredUsers });
+      db.close();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function renderFavorieten(req, res) {
+  const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+  client.connect((err, db) => {
+    if (err) throw err;
+
+    let favorietenCol = db.db('TechTeam').collection('favorieten');
+    let gebruikersCol = db.db('TechTeam').collection('gebruikers');
+
+    favorietenCol.findOne({ id: 0 }).then(results => {
+      // haal IDs van opgeslagen gebruikers op => geef hele object terug
+      let users = [];
+      results.opgeslagen.forEach(gebNaam => {
+        users.push(gebruikersCol.findOne({ naam: gebNaam }));
+      });
+
+      // nadat alle gebruikers in de user array zitten => render pagina
+      Promise.all(users)
+        .then(data => {
+          res.render('favorieten', { gebruikersLijst: data });
+          db.close();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  });
+}
+
+async function renderApi(req, res) {
   // --- fortnite API ---
   const fortniteApi = await fetch(
-    "https://fortnite-api.theapinetwork.com/items/list"
+    'https://fortnite-api.theapinetwork.com/items/list'
   )
-    .then((res) => res.json())
-    .then((json) => {
-      console.log("test");
+    .then(res => res.json())
+    .then(json => {
+      console.log('test');
 
       //GEGEVENS 0
       const naam0 = json.data[3].item.name;
@@ -172,7 +230,7 @@ async function handleApi(req, res) {
       const img4 = json.data[12].item.images.background;
       var array4 = [naam4, desc4, type4];
 
-      res.render("fortnite", {
+      res.render('fortnite', {
         array0: array0,
         img0: img0,
         array1: array1,
@@ -187,18 +245,15 @@ async function handleApi(req, res) {
     });
 }
 
-// filter post
-app.post("/zoeken", handleZoeken);
-
-// --- handle post ---
+// -- handle post --
 
 //als er een nieuwe oproep geplaatst wordt, wordt de variabel gebruiker gevuld
-app.post("/aanmelden", upload.single("image"), async (req, res) => {
+app.post('/aanmelden', upload.single('image'), async (req, res) => {
   //console.log(request.file);
   client.connect((err, db) => {
     if (err) throw err;
-    db.db("TechTeam")
-      .collection("gebruikers")
+    db.db('TechTeam')
+      .collection('gebruikers')
       .insertOne({
         naam: req.body.naam,
         leeftijd: req.body.leeftijd,
@@ -214,9 +269,9 @@ app.post("/aanmelden", upload.single("image"), async (req, res) => {
       })
       .then(() => {
         db.close();
-        res.redirect("/zoeken");
+        res.redirect('/zoeken');
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   });
@@ -237,6 +292,11 @@ function handleZoeken(req, res) {
 
 //filter optie
 function handleFilteren(req, res) {
+  const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
   //gekozen console optie door de gebruiker
   const consoleFilter = req.body.consolefilter;
 
@@ -248,7 +308,7 @@ function handleFilteren(req, res) {
     let query = {};
 
     //als gebruiker op de optie alle klikt wordt er een lege query verstuurd
-    if (consoleFilter === "Alle") {
+    if (consoleFilter === 'Alle') {
       query = {};
       //als de gebruiker een console kiest wordt de console uit de form in de query gezet
     } else {
@@ -258,8 +318,8 @@ function handleFilteren(req, res) {
     }
 
     //verbinding met db en de collectie
-    db.db("TechTeam")
-      .collection("gebruikers")
+    db.db('TechTeam')
+      .collection('gebruikers')
       //in de db wordt met de query gezocht
       .find(query)
       //resultaten worden in een array gezet
@@ -267,7 +327,7 @@ function handleFilteren(req, res) {
         if (err) throw err;
         console.log(gebruikers);
         //de gegevens worden gerenderd
-        res.render("zoeken", {
+        res.render('zoeken', {
           gebruikersLijst: gebruikers,
           consoleFilter,
         });
@@ -277,20 +337,18 @@ function handleFilteren(req, res) {
 }
 
 function handleFavorieten(req, res) {
-  console.log(testfavor);
   // reinstantiate client to prevent closed topology error
   const client = new MongoClient(uri, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
-  console.log("test");
 
   // voeg gebruikers ID aan favorieten toe
   let gebNaam = req.body.gebruikerNaam;
   console.log(gebNaam);
   client.connect(function (err, db) {
     if (err) throw err;
-    let favorietenCol = db.db("TechTeam").collection("favorieten");
+    let favorietenCol = db.db('TechTeam').collection('favorieten');
     favorietenCol
       .findOneAndUpdate({ id: 0 }, { $push: { opgeslagen: gebNaam } })
       .then(() => {
@@ -298,24 +356,47 @@ function handleFavorieten(req, res) {
       });
   });
   setTimeout(() => {
-    res.redirect("back");
+    res.redirect('back');
+  }, 70);
+}
+
+function handleFavorietenVerwijderen(req, res) {
+  // reinstantiate client to prevent closed topology error
+  const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+  // verwijder gebruikers ID van favorieten
+  let gebNaam = req.body.gebruikerNaam;
+  client.connect(function (err, db) {
+    if (err) throw err;
+    let favorietenCol = db.db('TechTeam').collection('favorieten');
+    favorietenCol
+      .findOneAndUpdate({ id: 0 }, { $pull: { opgeslagen: gebNaam } })
+      .then(() => {
+        db.close();
+      });
+  });
+  setTimeout(() => {
+    res.redirect('back');
   }, 70);
 }
 
 //wijzigingen doorvoeren
-app.post("/wijzigen", uploadWijzig.single("wijzigimage"), async (req, res) => {
+app.post('/wijzigen', uploadWijzig.single('wijzigimage'), async (req, res) => {
   try {
     //zoeken naar de juiste gebruiker aan de hand van de email die de gebruiker invoert
     client.connect((err, db) => {
       if (err) throw err;
-      db.db("TechTeam")
-        .collection("gebruikers")
+      db.db('TechTeam')
+        .collection('gebruikers')
         .findOneAndUpdate()
         .then(() => {
           db.close();
-          res.redirect("/zoeken");
+          res.redirect('/zoeken');
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
         });
     });
@@ -342,25 +423,25 @@ app.post("/wijzigen", uploadWijzig.single("wijzigimage"), async (req, res) => {
     //bij een error wordt de gebruiker doorverwezen naar de error pagina
   } catch (err) {
     console.log(err);
-    res.redirect("/error");
+    res.redirect('/error');
   }
 });
 
 //met deletemany worden alle records van de object verwijderd, aan de hand van de email
-app.post("/verwijderen", async (req, res) => {
+app.post('/verwijderen', async (req, res) => {
   try {
     await gebruiker.deleteMany({
       email: req.body.verwijderemail,
     });
-    res.redirect("/zoeken");
+    res.redirect('/zoeken');
   } catch (err) {
-    res.redirect("/error");
+    res.redirect('/error');
   }
 });
 
 //404
 app.use(function (req, res) {
-  res.status(404).render("404");
+  res.status(404).render('404');
 });
 
 //app geeft de port terug
